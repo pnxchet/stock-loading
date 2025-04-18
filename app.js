59a58f1bb7f549c792a2d5baecbb899f7f9d6031
Techin Bunphuttharak <techin@example.com>
2025-04-18 15:31:28 +0700

    remove comment

--- app.js
+++ app.js
@@ -2,12 +2,10 @@ const express = require('express');
 const app = express();
 const stockTaskRoutes = require('./routes/stockTaskRoutes');
 
-app.use(express.json()); // ใช้เพื่อรับ request body เป็น JSON
+app.use(express.json());
 
-// ใช้ routes สำหรับ stock tasks
 app.use('/api', stockTaskRoutes);
 
-// เริ่ม server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
